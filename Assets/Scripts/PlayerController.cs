1b7c2f6af3dac79d7e456e3e70fc7cf320ddc7e0
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-09-25 00:10:29 -0400

    part 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private void Start()
+	// Start is called before the first frame update
+	void Start()
 	{
 
 	}
 
-	private void Update()
+	// Update is called once per frame
+	void Update()
 	{
 		// Move the vehicle forward
+		transform.Translate(0, 0, 1);
 	}
 }

274d292ba32795d4da4e4ee32a783533ba427da3
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-10-01 09:45:06 -0400

    Part 5

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
 	public GameObject player;
+	public Vector3 offset = new Vector3(0, 5, -7);
 
 	void Start()
 	{
@@ -16,7 +17,7 @@ public class FollowPlayer : MonoBehaviour
 	// void OnPreRender()
 	void Update()
 	{
-		transform.position = player.transform.position + new Vector3(0, 5, -7);
+		transform.position = player.transform.position + offset;
 
 	}
 

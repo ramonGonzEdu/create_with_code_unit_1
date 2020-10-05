6a8eb6b4bd31acf63911f959c50b6b859c413687
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-10-05 09:21:04 -0400

    part 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed = 5.0f;
 	public float turnSpeed = 1.0f;
-	// public float horizontalInput;
+	public float horizontalInput;
 
 	// Start is called before the first frame update
 	void Start()
@@ -17,9 +17,9 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// horizontalInput = Input.GetAxis("Horizontal");
+		horizontalInput = Input.GetAxis("Horizontal");
 		// Move the vehicle forward
 		transform.Translate(Vector3.forward * Time.deltaTime * speed);
-		transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+		transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 	}
 }

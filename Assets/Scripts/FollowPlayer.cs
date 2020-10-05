f498494556fdbcdb379109d8d78d8293dca0ecdb
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-10-05 09:20:48 -0400

    step 2

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
 	public GameObject player;
 	public Vector3 offset = new Vector3(0, 5, -7);
 
+
 	void Start()
 	{
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float speed = 5.0f;
+	public float turnSpeed = 1.0f;
+	// public float horizontalInput;
 
 	// Start is called before the first frame update
 	void Start()
@@ -15,7 +17,9 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		// horizontalInput = Input.GetAxis("Horizontal");
 		// Move the vehicle forward
 		transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
 	}
 }
